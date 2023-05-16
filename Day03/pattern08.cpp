dd15958708798dc1aa48a22855cf54ddcd36e703
aditya0genius <61749196+aditya0genius@users.noreply.github.com>
2023-05-16 05:01:51 +0000

    Self try

---
+++ Day03/pattern08.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int row,column;
+    cout<<"Enter number of Rows";
+    cin>>row;
+    cout<<"Enter number of column";
+    cin>>column;
+    for(int i=1;i<=row;i++) {
+        for(int j=1;j<=column;j++)
+        {
+            if(i==1||j==1||i==row||j==column){
+                cout<<j;
+            }
+            else{
+                cout<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
