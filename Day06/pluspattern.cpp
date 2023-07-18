20d9eff71bfbac086a3fc7eea81e5fdea818abed
Aditya Deshmukh <adityadeshmukh168@gmail.com>
2023-07-18 20:19:12 +0530

    added cplus

--- Day06/pluspattern.cpp
+++ Day06/pluspattern.cpp
@@ -5,19 +5,21 @@ int main()
 {
 	
 	cout<<"Hello World";
-    int no_of_lines;
-    cout<<"Enter the no. of Lines";
+    int no_of_lines,mid;
+    cout<<"Enter the no. of Lines"<<endl;
     cin>>no_of_lines;
+    mid=no_of_lines/2;
     for(int i=0;i<no_of_lines;i++){
         if(no_of_lines%2!=0){
-            if(i=no_of_lines/2){
+            if(i==mid){
+                cout<<"\n";
                 for(int j=0;j<no_of_lines;j++)
                 {
                  cout<<"*";
                 }  
             }
             else{
-                cout<<"*";
+                cout<<"\n*";
             }
 
         }
